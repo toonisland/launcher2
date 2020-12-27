56ac45f4ee69892c67b9a6460ddfc8a55e86880c
Cranky Supertoon <CrankySupertoon@gmail.com>
2020-12-27 18:54:23 -0500

    finilize: code

--- src/game_starter.py
+++ src/game_starter.py
@@ -22,14 +22,13 @@ class GameStarter():
     def launchGame(self):
         # This still needs work but I guess this is slightly better
         cookie = str((self.uiCallback.uName + self.uiCallback.pWord))
-
+        GAME_SERVER = "127.0.0.1"
         ### Game starting commands ###
         TIA_PLAYCOOKIE = self.uiCallback.uName + self.uiCallback.pWord
-        if CURRENT_PLATFORM == 'Linux':
-            cmd_00 = 'export TIA_PLAYCOOKIE=' + self.uiCallback.uName + ' && export TTS_GAMESERVER=' + GAME_SERVER + self.python_path + ' -O -m toontown.toonbase.ToontownStart ' + cookie
-        else:
-            cmd_00 = 'set TIA_PLAYCOOKIE=' + self.uiCallback.uName + ' && set TTS_GAMESERVER=' + GAME_SERVER + self.python_path + ' -O -m toontown.toonbase.ToontownStart ' + cookie
-
+        if platform.system() == 'Windows':               
+            cmd_00 = 'SET TIA_PLAYCOOKIE=' + TIA_PLAYCOOKIE + ' && set TTS_GAMESERVER=' + GAME_SERVER + ' && TIAEngine.exe'
+        if platform.system() == 'Linux' or platform.system() == 'Darwin':
+            cmd_00 = 'export TIA_PLAYCOOKIE=' + TIA_PLAYCOOKIE + ' && export TTS_GAMESERVER=' + GAME_SERVER + ' && chmod +x TIAEngine && ./TIAEngine'
         # Before we run the command lets set the username variables to null
         self.uiCallback.uName = False
         self.uiCallback.pWord = False
